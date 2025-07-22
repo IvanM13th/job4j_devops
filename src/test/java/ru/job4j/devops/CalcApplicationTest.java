a7eb71957ed712deb339711f50dbc03a3bf84fe8
IvanM13th <bmchek13@gmail.com>
2025-07-22 11:58:38 +0500

    поправил тест

--- src/test/java/ru/job4j/devops/CalcApplicationTest.java
+++ src/test/java/ru/job4j/devops/CalcApplicationTest.java
@@ -3,6 +3,7 @@ package ru.job4j.devops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class CalcApplicationTests {
 
@@ -13,14 +14,5 @@ class CalcApplicationTests {
     @Test
     void mainMethodTest() {
         CalcApplication.main(new String[] {});
-    }
-
-    @Test
-    public void whenNegativeNumber() {
-        var input = new TwoArgs(-1, -1);
-        var expected = new Result(-2);
-        var output = new CalcController().summarise(input);
-        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
-        assertThat(output.getBody()).isEqualTo(expected);
     }
 }
