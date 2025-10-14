6fa4b13ba3c3b411a20d5374bf6ca1ee65eb8874
IvanM13th <bmchek13@gmail.com>
2025-10-14 23:42:57 +0500

    Использование кешей сборки в CI [#505355] - убрал коммент

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,3 @@
-/*buildCache {
-    remote<HttpBuildCache> {
-        url = uri(providers.gradleProperty("GRADLE_REMOTE_CACHE_URL"))
-        isAllowInsecureProtocol = true
-        isAllowUntrustedServer = true
-        val pushStr = providers.gradleProperty("GRADLE_REMOTE_CACHE_PUSH").orNull ?: "false"
-        isPush = pushStr.toBoolean()
-        credentials {
-            username = providers.gradleProperty("GRADLE_REMOTE_CACHE_USERNAME").orNull
-            password = providers.gradleProperty("GRADLE_REMOTE_CACHE_PASSWORD").orNull
-        }
-    }
-}*/
-
 buildCache {
     remote<HttpBuildCache> {
         fun getPropertyOrEnv(key: String): String? {
